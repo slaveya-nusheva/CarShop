44855a666bf47316936d570dee20bcde9ae81d69
s.nusheva <slaveyanusheva@gmail.com>
2023-12-20 15:47:49 +0200

    Initial commit

---
+++ src/carregistration/Car.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carregistration;
+
+/**
+ *
+ * @author smgOP682
+ */
+public class Car {
+    private String plateNumber;
+  
+   
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+    
+}
